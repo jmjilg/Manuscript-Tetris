acba9dccf29cfb9da03301730d27a00062c3a2b1
전민종 <giml790880@gmail.com>
2022-07-03 11:59:02 +0900

    Tetris 2 도형 클래스 설계 및 구성

---
+++ Shape.cpp
@@ -0,0 +1,67 @@
+#include "Shape.h"
+
+CShape::CShape()
+{
+	m_iWidthCount = 0;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			m_cShape[i][j] = '1';
+		}
+	}
+}
+
+CShape::~CShape()
+{
+}
+
+bool CShape::Init()
+{
+	m_tPivot.x = 0;
+	m_tPivot.y = 3;
+
+	return true;
+}
+
+void CShape::Render()
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			if (m_cShape[i][j] == '0')
+				cout << "бс";
+
+			else
+				cout << "  ";
+		}
+
+		cout << endl;
+	}
+}
+
+void CShape::MoveDown()
+{
+	if (m_tPos.y == STAGE_HEIGHT - 1)
+		return;
+
+	++m_tPos.y;
+}
+
+void CShape::MoveLeft()
+{
+	if (m_tPos.x == 0)
+		return;
+
+	--m_tPos.x;
+}
+
+void CShape::MoveRight()
+{
+	if (m_tPos.x + m_iWidthCount == STAGE_WIDTH)
+		return;
+
+	++m_tPos.x;
+}
---
+++ Shape.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "value.h"
+
+class CShape
+{
+public:
+	CShape();
+	virtual ~CShape();
+
+protected:
+	char		m_cShape[4][4];
+	POSITION	m_tPos;
+	POSITION	m_tPivot;
+	int			m_iWidthCount;
+
+public:
+	void SetPosition(int x, int y)
+	{
+		m_tPos.x = x;
+		m_tPos.y = y;
+	}
+
+	void SetPostion(const POSITION& tPos)
+	{
+		m_tPos = tPos;
+	}
+
+	POSITION GetPosition()	const
+	{
+		return m_tPos;
+	}
+
+	POSITION GetPivot()	const
+	{
+		return m_tPivot;
+	}
+
+public:
+	virtual bool Init();
+
+public:
+	void Render();
+	void MoveDown();
+	void MoveLeft();
+	void MoveRight();
+
+};
+
